bb5bf75cd79d5817316bac6eb13251a79e48a7f8
SKM <skm@Stevens-MacBook-Air.local>
2020-02-26 00:48:46 -0600

    code update

--- Counting Email in SQL.py
+++ Counting Email in SQL.py
@@ -5,6 +5,7 @@ from bokeh.plotting import figure, show, output_file
 from bokeh.models import ColumnDataSource
 from bokeh.palettes import Turbo256
 import traceback
+import itertools
 
 #Form SQL connection
 con = sqlite3.connect('emaildb.sqlite')
@@ -14,29 +15,37 @@ cur = con.cursor()
 output_file = 'EmailCount.html'
 df = pd.read_sql_query('SELECT org,count FROM Counts', con)
 df = df.dropna()
-idx = df['org'].tolist()
-idy = df['count'].tolist()
-idy.sort()
+x = df['org'].tolist()
+y = df['count'].tolist()
+
+#sort data in reverse order for pareto chart, create pareto_y variable
+y.sort(reverse=True)
+pareto_y = list(itertools.accumulate(y))
+
+#Check pareto total
+"""
+total = sum(y)
+print(total)
+"""
 
 #Check Dataframe 
 """
 #Check dataframe
 print(df)
 print(df.shape)
-print(idx)
-print(idy)
-
+print(x)
+print(y)
 """
 
 #Create vbar chart from dataframe
-source = ColumnDataSource(data=dict(idx=idx, idy=idy, color=Turbo256))
+source = ColumnDataSource(data=dict(x=x, y=y, color=Turbo256))
 
-p = figure(x_range=idx, plot_width=1900,
-           plot_height=500, title="Email Count by Organization",
+p = figure(x_range=x, plot_width=1900,
+           plot_height=750, title="Email Count by Organization",
            toolbar_location="left", tools="pan,reset,save,wheel_zoom")
 
-p.vbar(x='idx', top='idy', width=1, color="color", legend_field='idy',source=source)
-p.line(x=idx, y=idy, color="Chartreuse", line_width=2)
+p.vbar(x='x', top='y', width=1, color="color", legend_field='y',source=source)
+p.line(x=x, y=pareto_y, color="Chartreuse", line_width=2)
 
 p.x_range.range_padding = 0
 p.xgrid.grid_line_color = None
